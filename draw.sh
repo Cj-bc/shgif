b74fc1f644f577490d433328df6ab42eb9889f8f
Cj-bc <cj-bc@y-modify.org>
2018-10-07 14:24:58 +0900

    Add: draw> Draw::DrawAt

---
+++ draw.sh
@@ -0,0 +1,13 @@
+Draw::DrawAt() {
+  pos_x=$1
+  pos_y=$2
+  file=$3
+  declare -i i=1
+
+  tput cup $pos_y $pos_x
+  while IFS= read line; do
+    echo -n "$line"
+    tput cup $(( $pos_y + i)) $pos_x
+    i+=1
+  done < $file
+}
