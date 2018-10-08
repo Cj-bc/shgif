a620f7d2412913ac763c37c625e953bc52137063
Cj-bc <cj-bc@y-modify.org>
2018-10-08 17:40:54 +0900

    Change: tar> use $HOME instead of `/tmp` if `/tmp` does not exist

--- lib/tar.sh
+++ lib/tar.sh
@@ -4,9 +4,11 @@ Tar::Unpack() {
   local tarfile_path=$1
   local tarfile=${tarfile_path##*/}
   local -n pointer=$2
+  local tmp_head
 
   # make temporary dirs to expand, and move to there
-  local tempdir="/tmp/sh_tar_unpack.${tarfile}.$(date +%y%m%d%H%M).tmpdir"
+  test -d "/tmp" && tmp_head="/tmp" || tmp_head="$HOME"
+  local tempdir="${tmp_head}/sh_tar_unpack.${tarfile}.$(date +%y%m%d%H%M%S).tmpdir"
 
   # run in subshell so that `cd` will not affect outer codes.
   (
